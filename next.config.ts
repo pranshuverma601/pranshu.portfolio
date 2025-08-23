2da6e557b14426ebf736d8ca8d7bd8574eea2375
Pranshu Verma <143607053+pranshuverma601@users.noreply.github.com>
2025-08-23 14:13:01 +0530

    update the json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // enable static HTML export for Netlify
+  output: "export",
+
+  // disable built-in image optimization (not supported in static export)
+  images: {
+    unoptimized: true,
+  },
+
+  // optional: remove or set to true if you prefer
+  // trailingSlash: true,
 };
 
 export default nextConfig;
